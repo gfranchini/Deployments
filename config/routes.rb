6c9d184a600662191fad6533ee0b346292848a7a
Giancarlo Franchini <gfranchini88@gmail.com>
2016-11-16 20:51:17 +0000

    fixed db config

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :servers
   resources :forms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'forms#new'
 end
